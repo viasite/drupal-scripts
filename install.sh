6e5cc2b88f82ebc1cd9ab88a90eaad606d303502
popstas <popstas@gmail.com>
2015-03-27 01:00:47 +0500

    installer, moved scripts to bin/

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+PREFIX="$1"
+DRUPAL_SCRIPTS_ROOT="$PWD"
+USER_CONFIG_PATH="$HOME/.drupal-scripts.conf"
+
+if [ -z "$PREFIX" ]; then
+	echo "Usage: $0 <prefix>"
+	echo "  e.g. $0 /usr/local"
+	exit 1
+fi
+
+
+mkdir -p "$PREFIX"/bin
+ln -s "$DRUPAL_SCRIPTS_ROOT"/bin/* "$PREFIX"/bin
+#cp -R "$DRUPAL_SCRIPTS_ROOT"/bin/* "$PREFIX"/bin
+
+if [ ! -e "$USER_CONFIG_PATH" ]; then
+cp drupal-scripts.conf.example "$USER_CONFIG_PATH"
+else
+	echo "$USER_CONFIG_PATH exists"
+fi
+
+echo "Installed drupal-scripts to $PREFIX/bin"
