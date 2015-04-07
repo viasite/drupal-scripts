f0dd301e33fb0babe4eb65ac99f73833aeaa4e79
popstas <popstas@gmail.com>
2015-04-07 20:10:46 +0500

    assume yes for installer

--- install.sh
+++ install.sh
@@ -5,19 +5,42 @@ DEFAULT_PREFIX="/usr/share"
 BIN_PATH="/usr/local/bin"
 DRUPAL_SCRIPTS_ROOT="$PWD"
 USER_CONFIG_PATH="$HOME/.drupal-scripts.conf"
+SCRIPT_ASSUME_YES=""
 
 function usage(){
-	echo >&2 "Usage: $0 <prefix>"
-	echo >&2 "  e.g. $0 $DEFAULT_PREFIX"
+	echo "
+Usage: $0 <prefix>
+  e.g. $0 $DEFAULT_PREFIX
+
+Options:
+    -y Assume yes for all questions"
+
 }
 
-if [ ! -d "$PREFIX" ]; then
+if [ "$1" = "--help" ]; then
 	usage
-	read -p "Install drupal-scripts to $DEFAULT_PREFIX (y/n)? " -n 1 -r
-	echo ""
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		PREFIX="$DEFAULT_PREFIX"
+	exit 1
+fi
+
+while getopts ":y" opt; do
+	case "$opt" in
+		y) SCRIPT_ASSUME_YES="1"
+		shift
+		;;
+	esac
+done
+
+if [ ! -d "$PREFIX" ]; then
+	if [ -z "$SCRIPT_ASSUME_YES" ]; then
+		read -p "Install drupal-scripts to $DEFAULT_PREFIX (y/n)? " -n 1 -r
+		echo ""
+		if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+			echo "Aborting installation."
+			exit 1
+		fi
 	fi
+
+	PREFIX="$DEFAULT_PREFIX"
 fi
 
 if [ ! -d "$PREFIX" ]; then
@@ -28,15 +51,18 @@ fi
 INSTALL_DIR="$PREFIX/drupal-scripts"
 
 if [ -d "$INSTALL_DIR" ]; then
-	read -p "Previous installation detected. Replace $INSTALL_DIR (y/n)? " -n 1 -r
-	echo ""
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		rm -r "$INSTALL_DIR"
-		[ -h "$BIN_PATH"/drs ] && rm "$BIN_PATH"/drs
-	else
-		echo >&2 "Aborting installation."
-		exit 1
+	echo "Previous installation detected."
+	if [ -z "$SCRIPT_ASSUME_YES" ]; then
+		read -p "Replace $INSTALL_DIR (y/n)? " -n 1 -r
+		echo ""
+		if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+			echo "Aborting installation."
+			exit 1
+		fi
 	fi
+
+	rm -r "$INSTALL_DIR"
+	[ -h "$BIN_PATH"/drs ] && rm "$BIN_PATH"/drs
 fi
 
 cp -R "$DRUPAL_SCRIPTS_ROOT" "$INSTALL_DIR"
@@ -50,9 +76,10 @@ ln -s "$INSTALL_DIR"/bin/drs "$BIN_PATH"
 if [ ! -e "$USER_CONFIG_PATH" ]; then
 	cp drupal-scripts.conf.example "$USER_CONFIG_PATH"
 else
-	echo "$USER_CONFIG_PATH exists"
+	echo "$USER_CONFIG_PATH exists, don't rewrite it."
 fi
 
+# replace DRUPAL_SCRIPTS_ROOT path in config
 sed -i 's,DRUPAL_SCRIPTS_ROOT=.*,DRUPAL_SCRIPTS_ROOT="'"$INSTALL_DIR"'",g' "$USER_CONFIG_PATH"
 
 echo "Installed drupal-scripts to $INSTALL_DIR.
